addaeb067764a2071cd66d147d831217fee87fd7
Vladimir <vovangog83@gmail.com>
2020-05-29 16:22:35 +0300

    cicle for

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // a = 15;
 // console.log(a);
@@ -10,24 +10,24 @@
 // console.log(number);
 
 // const obj = {  
-//     name: "John",
+//     name: 'John',
 //     age: 25,
 //     isMaried: false
 // }
 // // obj.a = 10;
 // // console.log(obj.name);
-// console.log(obj["name"]);
+// console.log(obj['name']);
 
 // let arr = ['plum.png', 'orange.jpd', 6, 'apple.bmp', {}, []];
 // console.log(arr[1]);
 
 // alert('Hello');
 
-// const result = confirm("Are you here?");
+// const result = confirm('Are you here?');
 // console.log(result);
 
- const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
+ const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+ 
  const personalMovieDB = {
      count: numberOfFilms,
      movies: {},
@@ -35,11 +35,26 @@
      genres: [],
      privat: Boolean
     };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько оцените его?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const   a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }  else {
+        console.log('error');
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
 
 console.log(personalMovieDB);
